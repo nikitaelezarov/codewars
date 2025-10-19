9812487b423363de638d1cf328237d34475049de
nikitaelezarov <nikita.elezarov.01@mail.ru>
2025-10-19 19:01:15 +0300

    first commit

---
+++ 8kyu/Multiply.js
@@ -0,0 +1,19 @@
+//✨ DESCRIPTION
+// This code does not execute properly. Try to figure out why.
+
+
+//👀TESTS
+// assert.strictEqual(multiply(1,1), 1);
+// assert.strictEqual(multiply(2,1), 2);
+// assert.strictEqual(multiply(2,2), 4);
+// assert.strictEqual(multiply(3,5), 15);
+// assert.strictEqual(multiply(5,0), 0);
+// assert.strictEqual(multiply(0,5), 0);
+// assert.strictEqual(multiply(0,0), 0);
+
+//✔🐱‍🚀❤SOLUTION
+function multiply(a, b){
+    return a * b
+}
+
+console.log(multiply(2,2));
